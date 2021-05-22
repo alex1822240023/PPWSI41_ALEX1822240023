e70992a657a54a17bb966029a63b699b471e4b93
Alex <alextkj28@mhs.mdp.ac.id>
2021-05-22 18:49:05 +0700

    Buat API dan Controller(Resource)

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index(){
+        return array("pesan" => "Ini adalah home dari HomeController");
+    }
+
+    public function kenalan($nama = "Acuy"){
+        return array("pesan" => "Aku $nama");
+    }
+}
